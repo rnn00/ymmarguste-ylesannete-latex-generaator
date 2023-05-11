dbad5affd6313d0c54da1f9e12b2191fb9855a5c
rnn00 <109079846+rnn00@users.noreply.github.com>
2023-05-11 07:47:36 +0300

    muutsime Teema klassis lõpuks väljad finaliks

--- src/main/java/com/example/ymmargusteylesannetelatexgeneraator/Teema.java
+++ src/main/java/com/example/ymmargusteylesannetelatexgeneraator/Teema.java
@@ -3,11 +3,10 @@ package com.example.ymmargusteylesannetelatexgeneraator;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings({"CanBeFinal", "FieldMayBeFinal"})
 public class Teema {
     private final String nimi;
-    private List<Ülesanne> arvutusÜlesanded = new ArrayList<>();
-    private List<Ülesanne> tõestusÜlesanded = new ArrayList<>();
+    private final List<Ülesanne> arvutusÜlesanded = new ArrayList<>();
+    private final List<Ülesanne> tõestusÜlesanded = new ArrayList<>();
 
     public Teema(String nimi) {
         this.nimi = nimi;
